adc8e01dd9dc7aff5475b4fb89f1a4fa456328d9
Shaktijeet Sahoo <sahooshaktijeet@gmail.com>
2023-10-24 00:30:49 +0530

    add more fields and subtraction support

---
+++ index.js
@@ -0,0 +1,91 @@
+const moment = require('moment');
+
+function getDate(y, M, d, h, m, s, ms, format){
+    let now = moment();
+    
+    if (y >= 0) now.add(y, "y");
+    else now.subtract(-y, "y");
+
+    if (M >= 0) now.add(M, "M");
+    else now.subtract(-M, "M");
+
+    if (d >= 0) now.add(d, "d");
+    else now.subtract(-d, "d");
+
+    if (h >= 0) now.add(h, "h");
+    else now.subtract(-h, "h");
+    
+    if (m >= 0) now.add(m, "m");
+    else now.subtract(-m, "m");
+
+    if (s >= 0) now.add(s, "s");
+    else now.subtract(-s, "s");
+
+    if (ms >= 0) now.add(ms, "ms");
+    else now.subtract(-ms, "ms");
+
+    return now.format(format);
+}
+
+module.exports.templateTags = [{
+    name: 'Custom Timestamp 2',
+    displayName: 'CustomTimestamp',
+    description: 'Provides a formatted date with addition (use negative values for subtraction)',
+    args: [
+        {
+            displayName: 'Custom Format',
+            description: 'The output format of the date (default ISO8601)',
+            type: 'string',
+            defaultValue: "YYYY-MM-DDTHH:mm:ssZ" // default ISO8601
+        },
+        {
+            displayName: 'Milliseconds to add',
+            description: 'Milliseconds to add to the date.',
+            type: 'number',
+            defaultValue: 0
+        },
+        {
+            displayName: 'Seconds to add',
+            description: 'The seconds to add to the date.',
+            type: 'number',
+            defaultValue: 0
+        },
+        {
+            displayName: 'Minutes to add',
+            description: 'The minutes to add to the date.',
+            type: 'number',
+            defaultValue: 0
+        },
+        {
+            displayName: 'Hours to add',
+            description: 'The hours to add to the date.',
+            type: 'number',
+            defaultValue: 0
+        },
+        {
+            displayName: 'Days to add',
+            description: 'The days to add to the date.',
+            type: 'number',
+            defaultValue: 0
+        },
+        {
+            displayName: 'Months to add',
+            description: 'The months to add to the date.',
+            type: 'number',
+            defaultValue: 0
+        },
+        {
+            displayName: 'Years to add',
+            description: 'The years to add to the date.',
+            type: 'number',
+            defaultValue: 0
+        },
+    ].reverse(),
+    async run(context, days, months, years, format) {
+        return getDate(days, months, years, format);
+    },
+    liveDisplayName (context) {
+        let values = context.map(c => c.value);
+        return "CustomTimestamp (" + getDate(...values) + ")";
+    }
+}];
